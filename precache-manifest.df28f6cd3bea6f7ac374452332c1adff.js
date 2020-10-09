2d6080934d13e03bebc7ee4584ca0401bf3ce036
Rahul Patel <rahul.patel786@gmail.com>
2020-10-09 16:51:32 +0530

    Updates

---
+++ precache-manifest.df28f6cd3bea6f7ac374452332c1adff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b0fbb5447c57d49562a67ac11031db",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "de745982dc024170c435",
+    "url": "/me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "008875ef751bb7ddcceb",
+    "url": "/me/static/js/2.953b069d.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/me/static/js/2.953b069d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de745982dc024170c435",
+    "url": "/me/static/js/main.742da68b.chunk.js"
+  },
+  {
+    "revision": "e5cfc78d6897b9d69494",
+    "url": "/me/static/js/runtime-main.87a0448c.js"
+  }
+]);
